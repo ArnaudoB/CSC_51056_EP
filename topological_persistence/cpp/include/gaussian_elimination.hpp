d3b3991276b510caa46ca05be0aa369b35c5439e
ArnaudoB <baptiste.arnaudo@polytechnique.edu>
2025-10-20 22:02:49 +0200

    delete useless files

--- topological_persistence/cpp/include/gaussian_elimination.hpp
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include <cstdint>
-
-
-void gaussian_elim_dense(std::vector<std::vector<uint8_t>>& M);
-
-std::vector<std::vector<int>> gaussian_elim_sparse(std::vector<std::vector<int>> entries);
